8632551316dd5293adb3cffb45abb3c2e0447ff1
Jerzy Topola <jerzy.topola@gmail.com>
2025-04-24 20:40:55 +0200

    add decoder

--- read.py
+++ read.py
@@ -1,16 +1,27 @@
 '''
-This file is responsible for reading the information transmitted by the PiPico over serial.
-
-In further development this function will decode multiple inputs from the transmitted string.
+This file is responsible for reading the information transmitted by the Arduino Uno over serial. Please set correct COM port for your Arduino
 '''
+
 import serial
+port = 'COM4'
+#^^^ HERE CHANGE PORT TO THE ONE CORRESPONDING TO YOUR ARDUINO UNO
+unoSerial = serial.Serial(port=port, baudrate=9600)
 
-ser = serial.Serial(port='COM3', baudrate=9600)
+def getSerial():
+    unoRead = unoSerial.readline()
 
-def getValue():
-    value = ser.readline()
-    read = str(value, 'UTF-8')
+    #decode string to multiple pots
+    pots = str(unoRead, 'UTF-8').split(":")
+    return pots
 
-    volumeValue = float(read[0:4])
-    print(volumeValue)
-    return volumeValue
+def masterVolume():
+    return float(getSerial()[0])
+def pot1():
+    return float(getSerial()[1])
+def pot2():
+    return float(getSerial()[2])
+def pot3():
+    return float(getSerial()[3])
+def pot4():
+    #substracting '\r\n' from serial
+    return float(getSerial()[4][:-2])
