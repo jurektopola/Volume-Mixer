e823a8f32d7001a9e63dacebe5ae60fcf39fc5e3
Jerzy Topola <jerzy.topola@gmail.com>
2025-04-24 20:51:32 +0200

    add part potentiometer controll

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume, ISimpleAudioVolume
-from read import getValue
-
+from decode import masterVolume, pot1, pot2, pot3, pot4
 
 if __name__ == "__main__":
     devices = AudioUtilities.GetSpeakers()
     interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
     volume = cast(interface, POINTER(IAudioEndpointVolume))
 
-    #current = volume.GetMasterVolumeLevel()
-    volume.SetMasterVolumeLevel(0, None)
-
     while True:
+        volume.SetMasterVolumeLevel(masterVolume(), None)
         sessions = AudioUtilities.GetAllSessions()
         for sessions in sessions:
             TIDALvolume = sessions._ctl.QueryInterface(ISimpleAudioVolume)
-            if sessions.Process and sessions.Process.name() == "TIDALPlayer.exe":
-                TIDALvolume.SetMasterVolume(getValue(), None)
+            if sessions.Process and sessions.Process.name() == "TIDAL.exe":
+                TIDALvolume.SetMasterVolume(pot1(), None)
